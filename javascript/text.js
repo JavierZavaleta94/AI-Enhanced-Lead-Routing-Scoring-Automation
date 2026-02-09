64db3de50a36ca74c46cb2c7f2fea04acc11b124
Javier A Zavaleta <JavierX0149@gmail.com>
2026-02-09 00:41:41 -0500

    02.09.26 - Update link with parameter

--- javascript/text.js
+++ javascript/text.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (window.iframe) {
         const link = document.getElementById("myLink");
-        link.href = "https://javierzavaleta94.github.io/Hubspot-Workflow/?utm_medium=email&utm_campaign=test&utm_source=google&utm_content=Data%20Insight&utm_term=test";
+        link.href = "https://javierzavaleta94.github.io/AI-Enhanced-Lead-Routing-Scoring-Automation/?utm_medium=email&utm_campaign=test&utm_source=google&utm_content=Data%20Insight&utm_term=test";
         link.textContent = "Form Link with Parameters"; 
     }
 });
